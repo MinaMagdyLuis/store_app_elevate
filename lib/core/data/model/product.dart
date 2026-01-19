36c422d308436e41a28598939d649a20a091ce07
minamagdyluis <minaluis2017@gmil.com>
2026-01-19 14:36:23 +0200

    Initial commit

---
+++ lib/core/data/model/product.dart
@@ -0,0 +1,46 @@
+import 'rating.dart';
+import 'dart:convert';
+
+product storeResponseFromJson(String str) => product.fromJson(json.decode(str));
+String storeResponseToJson(product data) => json.encode(data.toJson());
+class product {
+  product({
+      this.id, 
+      this.title, 
+      this.price, 
+      this.description, 
+      this.category, 
+      this.image, 
+      this.rating,});
+  product.fromJson(dynamic json) {
+    id = json['id'];
+    title = json['title'];
+    price = json['price'];
+    description = json['description'];
+    category = json['category'];
+    image = json['image'];
+    rating = json['rating'] != null ? Rating.fromJson(json['rating']) : null;
+  }
+  num? id;
+  String? title;
+  num? price;
+  String? description;
+  String? category;
+  String? image;
+  Rating? rating;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['title'] = title;
+    map['price'] = price;
+    map['description'] = description;
+    map['category'] = category;
+    map['image'] = image;
+    if (rating != null) {
+      map['rating'] = rating?.toJson();
+    }
+    return map;
+  }
+
+}
